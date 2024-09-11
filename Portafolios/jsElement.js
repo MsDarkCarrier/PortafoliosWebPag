7a32d208d4401bcc6eb6c33afe3b601ff196b9b1
MsDarkCarrier <skeleton1353@gmail.com>
2024-09-11 17:12:46 -0600

    Resolición de muchos más problemas de merge

---
+++ Portafolios/jsElement.js
@@ -0,0 +1,33 @@
+const liSelector= document.querySelectorAll('.liHeader');
+
+liSelector.forEach(thumb =>{
+
+    thumb.addEventListener('click', function(){
+
+        if(!this.querySelector('.hidden')&& !this.querySelector('.textLetterColorDisable')){
+            
+            // Elimina las clases ocultas actuales
+
+            const imgDisable= document.querySelector('.hidden');
+            imgDisable.classList.remove('hidden');
+
+            const imgTextDisable= document.querySelector('.textLetterColorDisable');
+            imgTextDisable.classList.remove('textLetterColorDisable');
+
+
+            //Añade clases ocultas a nuevos objetos
+
+            const newImageDisable= this.querySelector('.activeImageSelector');
+            newImageDisable.classList.add('hidden');
+
+            const newImgTextDisable= this.querySelector('.textLetterColor');
+            newImgTextDisable.classList.add('textLetterColorDisable');
+
+
+
+        }
+
+
+
+    })
+})
